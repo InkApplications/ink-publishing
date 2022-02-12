99c6c735cb9e41e159226c89deebd94aba2913ac
Renee Vandervelde <Renee@ReneeVandervelde.com>
2022-02-12 12:13:23 -0600

    Configure POM after evaluate
    this prevents the gradle plugin from stripping the name and description.

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,33 +17,6 @@ gradlePlugin {
 }
 
 publishing {
-    publications {
-        withType<MavenPublication> {
-            pom {
-                name.set("Ink Publishing")
-                description.set("Boilerplate Gradle Publishing Config")
-                url.set("https://github.com/inkapplications/ink-publishing")
-                licenses {
-                    license {
-                        name.set("MIT")
-                        url.set("https://choosealicense.com/licenses/mit/")
-                    }
-                }
-                developers {
-                    developer {
-                        id.set("reneevandervelde")
-                        name.set("Renee Vandervelde")
-                        email.set("Renee@InkApplications.com")
-                    }
-                }
-                scm {
-                    connection.set("scm:git:https://github.com/InkApplications/ink-publishing.git")
-                    developerConnection.set("scm:git:ssh://git@github.com:InkApplications/ink-publishing.git")
-                    url.set("https://github.com/InkApplications/ink-publishing")
-                }
-            }
-        }
-    }
     repositories {
         maven {
             name = "Build"
@@ -59,6 +32,38 @@ publishing {
                 credentials {
                     username = mavenUser
                     password = mavenPassword
+                }
+            }
+        }
+    }
+}
+
+afterEvaluate {
+    extensions.configure(PublishingExtension::class.java) {
+        publications {
+            withType<MavenPublication> {
+                pom {
+                    name.set("Ink Publishing")
+                    description.set("Boilerplate Gradle Publishing Config")
+                    url.set("https://github.com/inkapplications/ink-publishing")
+                    licenses {
+                        license {
+                            name.set("MIT")
+                            url.set("https://choosealicense.com/licenses/mit/")
+                        }
+                    }
+                    developers {
+                        developer {
+                            id.set("reneevandervelde")
+                            name.set("Renee Vandervelde")
+                            email.set("Renee@InkApplications.com")
+                        }
+                    }
+                    scm {
+                        connection.set("scm:git:https://github.com/InkApplications/ink-publishing.git")
+                        developerConnection.set("scm:git:ssh://git@github.com:InkApplications/ink-publishing.git")
+                        url.set("https://github.com/InkApplications/ink-publishing")
+                    }
                 }
             }
         }
